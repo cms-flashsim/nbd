5c20e4230a1445861f820885218836b1a210d552
fcattafesta <filippo.cattafesta99@gmail.com>
2023-04-12 14:37:42 +0200

    Memory error debug #5

--- test.py
+++ test.py
@@ -11,16 +11,17 @@ from nbd.builder.nanomaker import nanomaker
 from nbd.builder.objs_dicts import objs_dicts
 
 if __name__ == "__main__":
-    root_nano = "/gpfs/ddn/srm/cms//store/mc/RunIIAutumn18NanoAODv6/DY2JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/"
-    new_root = "/gpfs/ddn/cms/user/cattafe/DYJets/EM1/"
-    files = [
-        os.path.join(d, f)
-        for d in os.listdir(root_nano)
-        for f in os.listdir(os.path.join(root_nano, d))
-    ]
+    # root_nano = "/gpfs/ddn/srm/cms//store/mc/RunIIAutumn18NanoAODv6/DY2JetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/Nano25Oct2019_102X_upgrade2018_realistic_v20-v1/"
+    # new_root = "/gpfs/ddn/cms/user/cattafe/DYJets/EM1/"
+    # files = [
+    #     os.path.join(d, f)
+    #     for d in os.listdir(root_nano)
+    #     for f in os.listdir(os.path.join(root_nano, d))
+    # ]
 
-    files_paths = [os.path.join(root_nano, f) for f in files]
-    files_paths = files_paths[:1]
+    # files_paths = [os.path.join(root_nano, f) for f in files]
+    # files_paths = files_paths[:1]
+    files_paths = ["~/43C42694-5B0A-7D47-B7E8-59249FFD69CD.root"]
     print(files_paths)
     print(f"We will process a total of {len(files_paths)} files")
 
