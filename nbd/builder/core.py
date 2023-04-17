184f3e0a46228b0bf89d51f532044f0ec0c0b7af
fcattafesta <filippo.cattafesta99@gmail.com>
2023-04-17 15:38:23 +0200

    debug unflatten

--- nbd/builder/core.py
+++ nbd/builder/core.py
@@ -77,7 +77,8 @@ def select_gen(
         print("Pileup columns not found in gen_columns")
         masked_gen = a_gen[gen_columns][a_gen["Mask"]]
 
-    reco_struct = ak.num(masked_gen, axis=1)
+    masked_gen.show(limit_cols=1000)
+    reco_struct = ak.num(masked_gen[], axis=1)
     print(f"Reco events: {reco_struct}")
     to_flash = ak.to_dataframe(masked_gen).reset_index(drop=True)
     # drop mask column
