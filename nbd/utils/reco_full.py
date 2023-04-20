09c50951f00f795e9de0b1b7d8c9469e81d1062b
fcattafesta <filippo.cattafesta99@gmail.com>
2023-04-20 14:33:21 +0200

    FullSim dedicated tree

--- nbd/utils/reco_full.py
+++ nbd/utils/reco_full.py
@@ -4,10 +4,12 @@ reco_objects = ["Electron", "Muon", "Jet"]
 
 
 def get_reco_columns(fields):
-    cols = np.array(
-        [
-            [var for var in fields if var.startswith(f"{obj}_") or var == f"n{obj}"]
-            for obj in reco_objects
-        ]
-    )
-    return cols.flatten()
+    cols = [
+        [var for var in fields if var.startswith(f"{obj}_") or var == f"n{obj}"]
+        for obj in reco_objects
+    ]
+
+    return list(np.concatenate(cols).flat)
+
+
+x = get_reco_columns(["Electron_pt", "Muon_pt", "Jet_pt"])
