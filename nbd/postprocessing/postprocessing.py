e3d063768534713e7be9d0b2e614ec13756efb08
fcattafesta <filippo.cattafesta99@gmail.com>
2023-04-22 16:12:11 +0200

    Eliminated

--- nbd/postprocessing/postprocessing.py
+++ nbd/postprocessing/postprocessing.py
@@ -45,10 +45,8 @@ def pi_minuspi_periodicity(df, column_name):
 
 
 def multiply_by_gen(df, gen_df, column_name, gen_column_name):
-    print("Multiplied!")
-    print(df[column_name])
     df[column_name] = df[column_name] * gen_df[gen_column_name]
-    print(df[column_name])
+
     return df[column_name]
 
 
