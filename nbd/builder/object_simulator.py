d10ee7d19e9f8806a23e14328888190e93132d76
fcattafesta <filippo.cattafesta99@gmail.com>
2023-04-24 10:17:18 +0200

    TStreamer warning debug #2

--- nbd/builder/object_simulator.py
+++ nbd/builder/object_simulator.py
@@ -74,7 +74,7 @@ def simulator(
 
     pt_col = [col for col in a_flash.fields if col.endswith("_pt")][0]
     # sort by pt
-    a_flash = a_flash[ak.argsort(a_flash[pt_col], axis=1)]
-    a_flash.show(limit_cols=1000)
+    # a_flash = a_flash[ak.argsort(a_flash[pt_col], axis=1)]
+
 
     return a_flash
