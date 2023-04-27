0720892ecf64284e40cbc83de5e7c08130a03f59
fcattafesta <filippo.cattafesta99@gmail.com>
2023-04-27 15:09:33 +0200

    GenPart is lastcopy

--- toys/match.py
+++ toys/match.py
@@ -19,11 +19,12 @@ rdf = ROOT.RDataFrame(events)
 
 rdf = (
     rdf.Define("GenPart_ElectronIdx_empty", "Electron_genObjMatchMaker(GenPart_pt)")
+    .Define("GenPart_isLastCopy", "GenPart_statusFlags & (1 << 13)")
     .Define(
         "GenPart_genElectron_ElectronIdx",
         "GenPart_ElectronIdx(GenPart_pt, GenPart_eta, GenPart_phi, GenPart_pdgId, GenPart_statusFlags, FullSim.Electron_pt, FullSim.Electron_eta, FullSim.Electron_phi, FullSim.Electron_charge, GenPart_ElectronIdx_empty, 0)",
     )
-    .Define("GenElectronMask", "abs(GenPart_pdgId) == 11")
+    .Define("GenElectronMask", "abs(GenPart_pdgId) == 11 && GenPart_isLastCopy")
     .Define("GenElectron_pt", "GenPart_pt[GenElectronMask]")
     .Define(
         "GenElectron_FullMatched",
