abab00b202fb057073d745f3055ebd1ca758a9d1
Francesco Vaselli <francesco.vaselli@protonmail.com>
2023-05-03 17:58:57 +0200

    no usless import

--- nbd/models/fatjets/fat_jets.py
+++ nbd/models/fatjets/fat_jets.py
@@ -29,10 +29,14 @@ from nflows.transforms import made as made_module
 # from nflows.transforms import splines
 from torch.nn.functional import softplus
 
-from modded_coupling import PiecewiseCouplingTransformM
-from modded_base_flow import FlowM
-from modded_splines import unconstrained_rational_quadratic_spline, rational_quadratic_spline
-import modded_splines
+from nbd.models.modded_nflows.modded_spline import (
+    unconstrained_rational_quadratic_spline,
+    rational_quadratic_spline,
+)
+# import .modded_nflows.modded_spline
+from nbd.models.modded_nflows import modded_spline
+from nbd.models.modded_nflows.modded_base_flow import FlowM
+
 
 
 class MaskedAffineAutoregressiveTransformM(AutoregressiveTransform):
@@ -217,105 +221,6 @@ class MaskedPiecewiseRationalQuadraticAutoregressiveTransformM(AutoregressiveTra
         return self._elementwise(inputs, autoregressive_params, inverse=True)
 
 
-class PiecewiseRationalQuadraticCouplingTransformM(PiecewiseCouplingTransformM):
-    def __init__(
-        self,
-        mask,
-        transform_net_create_fn,
-        num_bins=10,
-        tails=None,
-        tail_bound=1.0,
-        apply_unconditional_transform=False,
-        img_shape=None,
-        init_identity=True,
-        min_bin_width=modded_splines.DEFAULT_MIN_BIN_WIDTH,
-        min_bin_height=modded_splines.DEFAULT_MIN_BIN_HEIGHT,
-        min_derivative=modded_splines.DEFAULT_MIN_DERIVATIVE,
-    ):
-
-        self.num_bins = num_bins
-        self.min_bin_width = min_bin_width
-        self.min_bin_height = min_bin_height
-        self.min_derivative = min_derivative
-        self.tails = tails
-        self.tail_bound = tail_bound
-
-        if apply_unconditional_transform:
-            unconditional_transform = lambda features: PiecewiseRationalQuadraticCDF(
-                shape=[features] + (img_shape if img_shape else []),
-                num_bins=num_bins,
-                tails=tails,
-                tail_bound=tail_bound,
-                min_bin_width=min_bin_width,
-                min_bin_height=min_bin_height,
-                min_derivative=min_derivative,
-            )
-        else:
-            unconditional_transform = None
-
-        super().__init__(
-            mask,
-            transform_net_create_fn,
-            unconditional_transform=unconditional_transform,
-        )
-
-    def _transform_dim_multiplier(self):
-        if self.tails == "linear":
-            return self.num_bins * 3 - 1
-        else:
-            return self.num_bins * 3 + 1
-
-    def _piecewise_cdf(self, inputs, transform_params, inverse=False):
-        unnormalized_widths = transform_params[..., : self.num_bins]
-        unnormalized_heights = transform_params[..., self.num_bins : 2 * self.num_bins]
-        unnormalized_derivatives = transform_params[..., 2 * self.num_bins :]
-
-        if hasattr(self.transform_net, "hidden_features"):
-            unnormalized_widths /= np.sqrt(self.transform_net.hidden_features)
-            unnormalized_heights /= np.sqrt(self.transform_net.hidden_features)
-        elif hasattr(self.transform_net, "hidden_channels"):
-            unnormalized_widths /= np.sqrt(self.transform_net.hidden_channels)
-            unnormalized_heights /= np.sqrt(self.transform_net.hidden_channels)
-        else:
-            warnings.warn(
-                "Inputs to the softmax are not scaled down: initialization might be bad."
-            )
-
-        if self.tails is None:
-            spline_fn = rational_quadratic_spline
-            spline_kwargs = {}
-        else:
-            spline_fn = unconstrained_rational_quadratic_spline
-            spline_kwargs = {"tails": self.tails, "tail_bound": self.tail_bound}
-
-        return spline_fn(
-            inputs=inputs,
-            unnormalized_widths=unnormalized_widths,
-            unnormalized_heights=unnormalized_heights,
-            unnormalized_derivatives=unnormalized_derivatives,
-            inverse=inverse,
-            min_bin_width=self.min_bin_width,
-            min_bin_height=self.min_bin_height,
-            min_derivative=self.min_derivative,
-            **spline_kwargs
-        )
-
-
-def create_random_transform(param_dim):
-    """Create the composite linear transform PLU.
-    Arguments:
-        input_dim {int} -- dimension of the space
-    Returns:
-        Transform -- nde.Transform object
-    """
-
-    return transforms.CompositeTransform(
-        [
-            transforms.RandomPermutation(features=param_dim),
-            transforms.LULinear(param_dim, identity_init=True),
-        ]
-    )
-
 def create_base_transform(
     i,
     param_dim,
@@ -387,7 +292,7 @@ def create_base_transform(
         raise ValueError
 
     if base_transform_type == "rq-coupling":
-        return PiecewiseRationalQuadraticCouplingTransformM(
+        return PiecewiseRationalQuadraticCouplingTransform(
             mask=mask,
             transform_net_create_fn=(
                 lambda in_features, out_features: nn_.ResidualNet(
@@ -428,6 +333,20 @@ def create_base_transform(
     else:
         raise ValueError
 
+def create_random_transform(param_dim):
+    """Create the composite linear transform PLU.
+    Arguments:
+        input_dim {int} -- dimension of the space
+    Returns:
+        Transform -- nde.Transform object
+    """
+
+    return transforms.CompositeTransform(
+        [
+            transforms.RandomPermutation(features=param_dim),
+            transforms.LULinear(param_dim, identity_init=True),
+        ]
+    )
 
 def create_transform(
     num_flow_steps,
