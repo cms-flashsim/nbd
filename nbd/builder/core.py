439ba30e02c5358ff5f8e48a37082a0657de08de
Francesco Vaselli <francesco.vaselli@protonmail.com>
2023-05-04 15:25:26 +0200

    skip mask for fat_jets

--- nbd/builder/core.py
+++ nbd/builder/core.py
@@ -76,7 +76,10 @@ def select_gen(
         for col in pu:
             masked_gen[col] = a_gen[col]
     else:
-        masked_gen = a_gen[gen_columns][a_gen["Mask"]]
+        if eff:
+            masked_gen = a_gen[gen_columns][a_gen["Mask"]]
+        else:
+            masked_gen = a_gen[gen_columns]
 
     reco_struct = ak.num(masked_gen[gen_columns[0]], axis=1)
 
