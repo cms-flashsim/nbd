3ffb61394bfc27e0004a7d1019c5b85fdf670942
Francesco Vaselli <francesco.vaselli@protonmail.com>
2023-12-06 14:51:34 +0100

    added new input to fatjet model

--- nbd/extraction/fatjets/fatjets.h
+++ nbd/extraction/fatjets/fatjets.h
@@ -28,8 +28,7 @@ auto count_nHadrons(const ROOT::VecOps::RVec<float> &genh_eta,
   return count_h;
 }
 
-ROOT::VecOps::RVec<int> has_H_within_0_8(ROOT::VecOps::RVec<float> &genpart_eta, ROOT::VecOps::RVec<float> &genpart_phi, ROOT::VecOps::RVec<int> &genpart_id, ROOT::VecOps::RVec<float> &genjetAK8_eta, ROOT::VecOps::RVec<float> &genjetAK8_phi)
-{
+auto has_H_within_0_8(ROOT::VecOps::RVec<float> &genpart_eta, ROOT::VecOps::RVec<float> &genpart_phi, ROOT::VecOps::RVec<int> &genpart_id, ROOT::VecOps::RVec<float> &genjetAK8_eta, ROOT::VecOps::RVec<float> &genjetAK8_phi){
 
     int genpart_size = genpart_eta.size();
     int genjet_size = genjetAK8_eta.size();
@@ -43,7 +42,9 @@ ROOT::VecOps::RVec<int> has_H_within_0_8(ROOT::VecOps::RVec<float> &genpart_eta,
 
         for (int j = 0; j < genpart_size; j++)
         {
-            float dr = delta_R(genjetAK8_eta[i], genjetAK8_phi[i], genpart_eta[j], genpart_phi[j]);
+            Double_t deta = genjetAK8_eta[i] - genpart_eta[j];
+            Double_t dphi = TVector2::Phi_mpi_pi(genjetAK8_phi[i] - genpart_phi[j]);
+            float dr = TMath::Sqrt(deta * deta + dphi * dphi);
             if (dr < dr_0 && genpart_id[j] ==25)
             is_h_within_0_8[i] = 1;
 
